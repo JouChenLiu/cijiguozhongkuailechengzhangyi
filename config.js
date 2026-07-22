8cff64cf12599135896497b228e01e886dbd8605
JouChenLiu <ann987987987@gmail.com>
2026-07-23 02:44:39 +0800

    new file

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js (這個檔案絕對不能上傳到公開的 GitHub)
+const APP_CONFIG = {
+    EMAILJS_PUBLIC_KEY: "T5jUhJh3JQT2a7kUr",
+    FIREBASE: {
+        apiKey: "AIzaSyAIMqhpPWrjUE_SDfLXKLt7B4ryn8G4G1U",
+        authDomain: "cijiguozhongkuailechengz-26e8d.firebaseapp.com",
+        projectId: "cijiguozhongkuailechengz-26e8d",
+        databaseURL: "https://cijiguozhongkuailechengz-26e8d-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    }
+};
